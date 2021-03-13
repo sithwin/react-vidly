50a7798a751e8f4a1cc3a4a5fbc84e71d9231a61
Si Thu Win <sthuwin@gmail.com>
2021-03-13 14:04:17 +0800

    render list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World</h1>
+      <h1>Header</h1>
+      <Movies />
     </main>
   );
 }
