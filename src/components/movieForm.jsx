de6a750339716cfd2d7c74c1aacaddc9661786d0
Si Thu Win <sthuwin@gmail.com>
2021-03-24 23:23:36 +0800

    added new movie form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,29 +1,62 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import { min } from "lodash";
+import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 
 class MovieForm extends Form {
   state = {
     data: {
       title: "",
-      genre: "",
-      stock: 0,
-      rate: 0.0,
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: "",
     },
+    genres: [],
     errors: {},
   };
 
   schema = {
+    _id: Joi.string(),
     title: Joi.string().required().label("Title"),
-    genre: Joi.string().required().label("Genre"),
-    stock: Joi.number().required().label("Number in Stock"),
-    rate: Joi.number().required().min(3).max(10).label("Rate"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .integer()
+      .min(0)
+      .max(100)
+      .required()
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number().required().min(0).max(10).label("Rate"),
   };
+
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
+
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
+
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
+      title: movie.title,
+      genreId: movie.genres._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate,
+    };
+  }
 
   doSubmit = () => {
     // Call the server
+    saveMovie(this.state.data);
     console.log("Submitted", this.state.data);
+    this.props.history.push("/movies");
   };
 
   render() {
@@ -32,8 +65,8 @@ class MovieForm extends Form {
         <h1>Movie Form</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("title", "Title")}
-          {this.renderInput("genre", "Genre")}
-          {this.renderInput("stock", "Number in Stock", "number")}
+          {this.renderSelect("genreId", "Genre")}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
           {this.renderInput("rate", "Rate", "number")}
           {this.renderButton("Save")}
         </form>
